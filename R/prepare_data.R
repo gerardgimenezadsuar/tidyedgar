946dbc5934219b68b4e9755acf450e592f8f9485
gerardgimenezadsuar <48365704+gerardgimenezadsuar@users.noreply.github.com>
2024-01-30 18:07:00 +0100

    .

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -1,14 +1,46 @@
 #' Data wrangling for tidy fundamental data from EDGAR
 #'
-#' @param df A dataframe.
-#' @param ... Additional dataframes passed.
+#' @param df A dataframe, output from get_qdata() or get_ydata().
+#' @param ... Additional dataframes to be combined from other accounts (NetIncomeLoss, OperatingIncomeLoss, etc).
 #' @param quarterly Boolean indicating if quarterly data is present.
 #' @import dplyr
 #' @import tidyr
 #' @returns A dataframe
 #' @export
 #' @examples
-#' prepare_data(get_qdata(account = "NetIncomeLoss", years = 2022:2023, quarters = c("Q4")))
+#' revenue <- data.frame(
+#'taxonomy = rep("us-gaap", 3),
+#'tag = rep("Revenues", 3),
+#'ccp = rep("CY2020", 3),
+#'uom = rep("USD", 3),
+#'label = rep("Revenues", 3),
+#'description = rep("Amount of revenue recognized from goods sold, services rendered, ...", 3),
+#'pts = rep(2762, 3),
+#'data.accn = c("0001564590-22-012597", "0000002178-23-000038", "0001654954-22-005679"),
+#'data.cik = c(2098, 2178, 2186),
+#'data.entityName = c("ACME CORP", "ADAMS RESOURCES, INC.", "BK TECHNOLOGIES"),
+#'data.loc = c("US-CT", "US-TX", "US-FL"),
+#'data.start = rep("2020-01-01", 3),
+#'data.end = rep("2020-12-31", 3),
+#'data.val = c(164003040, 1022422000, 44139000),
+#'year = rep(2020, 3))
+#'netincome <- data.frame(
+#'taxonomy = rep("us-gaap", 3),
+#'tag = rep("NetIncomeLoss", 3),
+#'ccp = rep("CY2020", 3),
+#'uom = rep("USD", 3),
+#'label = rep("NetIncomeLoss", 3),
+#'description = rep("Net Income from operating activities", 3),
+#'pts = rep(2762, 3),
+#'data.accn = c("0001564590-22-012597", "0000002178-23-000038", "0001654954-22-005679"),
+#'data.cik = c(2098, 2178, 2186),
+#'data.entityName = c("ACME CORP", "ADAMS RESOURCES, INC.", "BK TECHNOLOGIES"),
+#'data.loc = c("US-CT", "US-TX", "US-FL"),
+#'data.start = rep("2020-01-01", 3),
+#'data.end = rep("2020-12-31", 3),
+#'data.val = c(100000, 200000, 4000000),
+#'year = rep(2020, 3))
+#'prepare_data(revenue,netincome, quarterly = FALSE)
 
 prepare_data <- function(df = NULL, quarterly = TRUE, ...) {
   if (!is.data.frame(df)) {
@@ -53,10 +85,10 @@ prepare_data <- function(df = NULL, quarterly = TRUE, ...) {
       arrange(.data$data.entityName, .data$year) %>%
       group_by(.data$data.cik) %>%
       mutate(
-        qoq_change_NI = if("NetIncomeLoss" %in% existing_cols) (.data$NetIncomeLoss - lag(.data$NetIncomeLoss)) / abs(lag(.data$NetIncomeLoss)) else NA,
-        qoq_change_R = if("revenue" %in% existing_cols) (.data$revenue - lag(.data$revenue)) / abs(lag(.data$revenue)) else NA,
-        qoq_change_OI = if("OperatingIncomeLoss" %in% existing_cols) (.data$OperatingIncomeLoss - lag(.data$OperatingIncomeLoss)) / abs(lag(.data$OperatingIncomeLoss)) else NA,
-        qoq_change_R = ifelse(is.infinite(.data$qoq_change_R), 0, .data$qoq_change_R),
+        change_NI = if("NetIncomeLoss" %in% existing_cols) (.data$NetIncomeLoss - lag(.data$NetIncomeLoss)) / abs(lag(.data$NetIncomeLoss)) else NA,
+        change_R = if("revenue" %in% existing_cols) (.data$revenue - lag(.data$revenue)) / abs(lag(.data$revenue)) else NA,
+        change_OI = if("OperatingIncomeLoss" %in% existing_cols) (.data$OperatingIncomeLoss - lag(.data$OperatingIncomeLoss)) / abs(lag(.data$OperatingIncomeLoss)) else NA,
+        change_R = ifelse(is.infinite(.data$change_R), 0, .data$change_R),
         operating_margin = if("OperatingIncomeLoss" %in% existing_cols & "revenue" %in% existing_cols) round(.data$OperatingIncomeLoss / .data$revenue, 4) else NA,
         net_margin = if("NetIncomeLoss" %in% existing_cols & "revenue" %in% existing_cols) round(.data$NetIncomeLoss / .data$revenue, 4) else NA
       ) %>%
